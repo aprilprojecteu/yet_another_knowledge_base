a1d3b1af2afb5c7f41196765c633afb538c15bd3
Marian Renz <marian.renz@dfki.de>
2022-04-26 11:07:20 +0200

    Do some formatting

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 
-## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
+# ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
 
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 # for your packages to be recognized by python
 setup_args = generate_distutils_setup(
-  packages=['yet_another_knowledge_base'],
-  package_dir={'yet_another_knowledge_base': 'common/src/yet_another_knowledge_base'}
+    packages=['yet_another_knowledge_base'],
+    package_dir={
+        'yet_another_knowledge_base': 'common/src/yet_another_knowledge_base'}
 )
 
 setup(**setup_args)
